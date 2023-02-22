cabf9f0df303d8338f434f74348ea697c8c28ee6
Kristian Popov <kristianpopov03@yahoo.com>
2023-02-23 00:04:35 +0100

    Add files via upload

---
+++ Calculator-chatGPT/script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById('result');
+
+function appendToDisplay(value) {
+    display.value += value;
+}
+
+function clearDisplay() {
+    display.value = '';
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (e) {
+        display.value = 'Error';
+    }
+}
